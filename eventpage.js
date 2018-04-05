63c56eb2f6597038b7b68b78a635ddcdd9f5ba6f
Daniel Swensson <daniel@danielswensson.se>
2018-04-05 21:35:09 +0200

    Base tone on avg charcode of request URL

--- eventpage.js
+++ eventpage.js
@@ -1,9 +1,26 @@
 var analyticsCount = 0;
+
+function sendMusicMessage(avgCharCode) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(
+      tabs[0].id, {
+        analyticsCount: analyticsCount,
+        avgCharCode: avgCharCode,
+      },
+      null
+    );
+  });
+}
+
 chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { analyticsCount:  ++analyticsCount }, null);
-    });
+  function (request) {
+    const url = request.url
+    var charSum = 0;
+    for(var i = 0; i < url.length; i++) {
+      charSum += url.charCodeAt(i);
+    }
+    analyticsCount++;
+    sendMusicMessage(charSum / url.length);
   },
   {urls: ["*://*.google-analytics.com/*"]}
 )
--- music.js
+++ music.js
@@ -1,36 +1,24 @@
 var baseFrequency = 110;
-var crusher = new Tone.Distortion(0.5).toMaster();
-var pingPong = new Tone.PingPongDelay(0.4, 0.05).connect(crusher);
-crusher.set("wet", 0);
-var synth = new Tone.PolySynth(2, Tone.Synth).connect(pingPong);
+
+var pingPong = new Tone.PingPongDelay(0.4, 0.05).toMaster();
+
+var filter = new Tone.AutoFilter(2, 200, 4).connect(pingPong);
+filter.set("wet", 0.4);
+var synth = new Tone.FMSynth().connect(filter, 0);
 
 function instrument(analyticsCount) {
-  if (analyticsCount > 20) {
-    crusher.set("wet", 0.5);
-  }
   return synth;
 }
 
-function frequency(analyticsCount) {
-  var randomMulti = Math.floor(Math.random() * analyticsCount + 1); 
-  var randomMinMulti = Math.floor(Math.random() * 7 + 1);
-  var randFreq = baseFrequency * Math.min(randomMinMulti, randomMulti);
-  if (analyticsCount > 10) {
-    return [randFreq, randFreq * 1.5];
-  }
-  return randFreq;
-}
-
-function triggerSound(analyticsCount) {
+function triggerSound(analyticsCount, avgCharCode) {
   var velocity = Math.max(0.5, Math.random());
-  pingPong.set("wet", Math.random());
-  var freq = frequency(analyticsCount);
-  console.log(freq);
+  pingPong.set("wet", 0.4);
+  var freq = avgCharCode * Math.floor(Math.random() * 7 + 1);
   instrument(analyticsCount).triggerAttackRelease(freq, 0.1, undefined, velocity);
 }
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    triggerSound(request.analyticsCount);
+    triggerSound(request.analyticsCount, request.avgCharCode);
   }
 );
